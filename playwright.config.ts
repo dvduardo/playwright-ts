25510d1bea1dea5deada52269c7da21872f1ddf4
David <dvduardo@gmail.com>
2024-06-12 23:21:24 -0300

    Adding videos on report

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,8 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     headless: true,
-    baseURL: process.env.BASE_URL
+    baseURL: process.env.BASE_URL,
+    video: 'on'
   },
 
   /* Configure projects for major browsers */
